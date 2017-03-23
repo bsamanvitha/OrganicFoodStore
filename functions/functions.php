f6a4d6257e4ad3261f88a04ac376026cda475640
Samanvithab <s97basole@gmail.com>
2017-03-23 12:52:37 -0700

    Update getProducts php function

--- functions/functions.php
+++ functions/functions.php
@@ -27,24 +27,28 @@ function getCats(){
 	}
 }
 
-//getting the products; METHOD WILL BE USED to display products
 function getProducts(){
 	global $con; //will make it work inside this function
 
 	//local variable always created w/ the $ sign
 	//written an sql query
-	$get_cats = "select * from categories";
+	$get_products = "select * from products order by RAND() LIMIT 0,6";
 	//run this query
-	$run_cats = mysqli_query($con, $get_cats); //2nd field: function that has the query
+	$run_products = mysqli_query($con, $get_products); //2nd field: function that has the query
 
 	//will retrieve all of the records from the table
 	//we are fetching this query run_cats using mysqli_fetch_array
 	//we are saving the data to the local variable row_cats
-	while($row_cats=mysqli_fetch_array($run_cats)){
-		$cat_id = $row_cats['cat_id']; //get the id from the table
-		$cat_title = $row_cats['cat_title']; // get title from the table
+	while($row_products=mysqli_fetch_array($run_products)){
+		$product_id = $row_products['product_id']; //get the id from the table
+		$product_title = $row_cats['product_title']; // get title from the table
+		$product_image = $row_products['product_image'];
+		$product_desc = $row_products['product_desc'];
+		$product_cat = $row_products['product_cat'];
+		$product_price = $row_products['product_price'];
+		$product_keywords = $row_products['product_keywords'];
 		
-		echo "<a href='#'>$cat_title</a>";
+		echo "<a href='#'>$product_title</a>";
 	
 	
 	}
