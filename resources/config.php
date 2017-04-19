678d19807244b7e8c207cbf494895d61ae39038f
Samanvithab <s97basole@gmail.com>
2017-04-19 11:52:57 -0700

    p

--- resources/config.php
+++ resources/config.php
@@ -23,22 +23,17 @@ defined("") ? null : define("", "root");
 
 defined("database") ? null : define("database",  "ecom");
 
-#$connection = mysqli_connect("localhost","root","","database");
+$connection = mysqli_connect("localhost","root","","database");
 #uncomment the above for local connection
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+#$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+#$server = $url["host"];
+#username = $url["user"];
+#$password = $url["pass"];
+#$db = substr($url["path"], 1);
 
 
-$connection = array(
-    'host' => $server ,
-    'user' => $username ,
-    'pw' => $password,
-    'db' => $db 
-);
+#$connection = array('host' => $server , 'user' => $username , 'pw' => $password, 'db' => $db );
 
 require_once("functions.php");
 require_once("cart.php");
