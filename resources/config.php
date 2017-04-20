567b732e0edac9a5fe0fd48a2042c993b0b76844
Samanvithab <s97basole@gmail.com>
2017-04-20 09:08:19 -0700

    Heroku

--- resources/config.php
+++ resources/config.php
@@ -26,7 +26,7 @@ defined("database") ? null : define("database",  "ecom");
 #$connection = mysqli_connect("localhost","root","","database");
 #uncomment the above for local connection
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("mysql://bad8f37e5eda46:8035b0f5@us-cdbr-iron-east-03.cleardb.net/heroku_5003a71979fc0af?reconnect=true"));
 $server = $url["us-cdbr-iron-east-03.cleardb.net’"];
 $username = $url["bad8f37e5eda46"];
 $password = $url["8035b0f5"];
